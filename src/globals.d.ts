c99e478f70bbb2f231f49261a5a8b02d2d7fabd8
unknown <amaleekadedotun@egmail.com>
2025-03-02 14:41:27 -0800

    first commit

---
+++ src/globals.d.ts
@@ -0,0 +1,6 @@
+// src/globals.d.ts or src/types.d.ts
+declare module "*.jsx" {
+    const content: any;
+    export default content;
+  }
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// // /** @type {import('tailwindcss').Config} */
+// // export default {
+// //   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// //   theme: {
+// //     extend: {},
+// //   },
+// //   plugins: [require("daisyui")],
+// // };
+// module.exports = {
+//   content: [
+//     './index.html', 
+//     './src/**/*.{js,ts,jsx,tsx}', // adjust paths based on your file structure
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    "./index.html",  // Path to HTML files (or templates)
+    "./src/**/*.{js,ts,jsx,tsx}",  // Path to JavaScript or TypeScript files
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
+
